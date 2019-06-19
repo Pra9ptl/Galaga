8336e7894e4f9129a8d6253ca4d2054ca40f6499
Patel Pranav <patelpranav1313@gmail.com>
2019-06-19 08:34:37 -0400

    Initial Commit

---
+++ Galaga/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Galaga
+//
+//  Created by Harsh Parikh on 2019-06-19.
+//  Copyright © 2019 Pranav Patel. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
